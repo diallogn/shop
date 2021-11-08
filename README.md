80c1f28821d1ded04cdf0031278c41508f4128c2
Abdoulaye Diallo <dialloabdoulaye71@gmail.com>
2021-11-08 15:03:55 +0000

    create theme

--- README.md
+++ README.md
@@ -8,3 +8,8 @@
    3. Show product info in second column
    4. Show add to cart action on third column
    5. Add styles
+10. Add MaterialUI Theme
+    1. Create theme
+    2. Use theme provider
+    3. add h1 and h2 styles
+    4. set Theme colors
--- components/Layout.js
+++ components/Layout.js
@@ -7,9 +7,38 @@ import {
   Toolbar,
   Typography,
   Link,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
 } from '@material-ui/core';
 
 export default function Layout({ title, description, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      body1: {
+        fontWeight: 'normal',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
   const styles = useStyles();
   return (
     <>
@@ -17,24 +46,27 @@ export default function Layout({ title, description, children }) {
         <title>{title ? `${title} - Amazona` : 'Amazona'}</title>
         {description && <meta name="description" content={description} />}
       </Head>
-      <AppBar position="static" className={styles.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={styles.brand}>amazona</Typography>
-            </Link>
-          </NextLink>
-          <div className={styles.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={styles.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={styles.brand}>amazona</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
+            <div className={styles.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
       <Container className={styles.main}>{children}</Container>
       <footer className={styles.footer}>
         <Typography>All right reserved. Next Amazona.</Typography>
