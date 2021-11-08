6359716342f6b1470fd63ebd16e280c5f8842d9a
Abdoulaye Diallo <dialloabdoulaye71@gmail.com>
2021-11-08 12:54:32 +0000

    product detail router

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import {
   CardActions,
   Button,
 } from '@material-ui/core';
+import NextLink from 'next/link';
 import data from '../utils/data';
 
 export default function Home() {
@@ -20,16 +21,18 @@ export default function Home() {
           return (
             <Grid item md={4} key={product.name}>
               <Card>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    image={product.image}
-                    title={product.name}
-                  ></CardMedia>
-                  <CardContent>
-                    <Typography>{product.name}</Typography>
-                  </CardContent>
-                </CardActionArea>
+                <NextLink href={`/product/${product.slug}`}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
                 <CardActions>
                   <Typography>$ {product.price}</Typography>
                   <Button size="small" color="primary">
